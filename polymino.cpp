612289c9e6c1c6e6fdcac458f9ba2acfd3cd8053
sminux <espin55@yandex.ru>
2016-12-21 18:18:01 +0300

    correct fake_figure search

--- polymino.cpp
+++ polymino.cpp
@@ -37,83 +37,35 @@ polymino::polymino(f *figures, int n)
 
 size_t **polymino::mergeFigures(size_t **first_figure, int r1, int c1, size_t ** second_figure, int r2, int c2)
 {
-	size_t **record = new size_t*[r1];
-	for (size_t i = 0; i < r1; i++) record[i] = new size_t[c1];
+	f prob_f(first_figure, r1, c1);
+	prob_f.findAngle();//здесь ошибка
 
-	int row_length = 0, column_length = 0, new_row_length, new_column_length;
-	int record_r, record_c;
+	/*size_t **additional_arr = new size_t*[r1 + r2 - 1]; 
+	for (int i = 0; i < r1 + r2; i++) additional_arr[i] = new size_t[c1 + c2 - 1];
+	for (int i(0); i < r1 + r2 - 1; i++)
+		for (int j(0); j < c1 + c2 - 1; j++) additional_arr[i][j] = 1;*/
 
-	for (int i = 0; i < 4; i++)
-	{
-		new_row_length = 0; new_column_length = 0;
-		if ((c1 > 1) && (r1 > 1))		//else we have no angle
-		{
-			if ((first_figure[0][0] == 1) && (first_figure[0][0] == first_figure[0][1]) && (first_figure[0][0] == first_figure[1][0]))
-			{
+	rows = prob_f.get_r();
+	colums = prob_f.get_c();
 
-				for (int Y = 0; Y < c1; Y++)
-				{
-					if (first_figure[0][Y] == 1)new_column_length++;
-				}
+	size_t **fake_figure = new size_t*[rows];
+	for (int i = 0; i < rows; i++) fake_figure[i] = new size_t[colums];
 
-				for (int Y = 0; Y < r1; Y++)
-				{
-					if (first_figure[Y][0] == 1)new_row_length++;
-				}
-
-
-				if ((new_row_length >= row_length) && (new_column_length >= column_length))
-				{
-					for (int i(0); i < r1; i++)
-					{
-						for (int j(0); j < c1; j++)
-							record[i][j] = first_figure[i][j];
-					}
-					record_c = c1;
-					record_r = r1;
-					row_length = new_row_length;
-					new_column_length = new_column_length;
-				}
-			}
-			int n = 0;
-			(r1 > c1) ? n = r1 : n = c1;	//it's easy to ratate square matrix
-
-			for (size_t i = 0; i < n; ++i)
-				for (size_t j = i; j < n; ++j)		//beggining from [i] 'cause transponation over main diagonal
-					swap(first_figure[i][j], first_figure[j][i]);
-
-			swap(r1, c1);
-			for (size_t i = 0; i < c1 / 2; i++)
-			{
-				for (size_t j = 0; j < r1; j++) swap(first_figure[j][i], first_figure[j][c1 - i - 1]);
-			}
-			hint += "R";
-		}
-
-	}
-
-	first_figure = record;
-	c1 = record_c; r1 = record_r;
-
-	size_t **additional_arr = new size_t*[r1 + r2]; 
-	for (int i = 0; i < r1 + r2; i++) additional_arr[i] = new size_t[c1 + c2];
-	for (int i(0); i < r1 + r2; i++)
-		for (int j(0); j < c1 + c2; j++) additional_arr[i][j] = 1;
-
-	size_t **fake_figure = new size_t*[r1];
-	for (int i = 0; i < r1; i++) fake_figure[i] = new size_t[c1];
-
-	fake_figure = first_figure;
+	fake_figure = prob_f.array();
 
 	int units = 0;
 	for (int i(0); i < r2; i++)
 	{
 		for (int j(0); j < c2; j++)
+		{ 
 			if (second_figure[i][j] == 1)units++;
+			cout << second_figure[i][j] << ' ';
+		}
+		cout << endl;
 	}
+	
 
-
-	for (int i(0); i < r1; i++)
+	/*for (int i(0); i < r1; i++)
 	{
 		for (int j(0); j < c1; j++)
 		{
@@ -124,36 +76,37 @@ size_t **polymino::mergeFigures(size_t **first_figure, int r1, int c1, size_t **
 				additional_arr[i][j] = 1;
 			}
 		}
-	}
+	}*/
+
 	bool point = true;		//to check the first equivalention
 	/*serch for 2nd massiv in fake_figure*/
-	for (int i(0); i < r1; i++)
+	for (int i(0); i < rows - r2 + 1; i++)
 	{
-		for (int j(0); j < c1; j++)
+		for (int j(0); j < colums - c2 + 1; j++)
 		{
 			int koefficient_of_similarity = 0;
-			for (int subarrayX = i; subarrayX < i + r2; subarrayX++)
+
+			for (int subarrayX = 0; subarrayX < r2; subarrayX++)
 			{
-				for (int subarrayY = j; subarrayY < j + c2; subarrayY++)
+				for (int subarrayY = 0; subarrayY < c2; subarrayY++)
 				{
-					if ((fake_figure[subarrayX][subarrayY] == 0) && (second_figure[subarrayX - i][subarrayY - j] == 1)) koefficient_of_similarity++;
+					if ((fake_figure[subarrayX + i][subarrayY + j] == 0) && (second_figure[subarrayX][subarrayY] == 1)) koefficient_of_similarity++;
 				}
 			}
-
 			if ((koefficient_of_similarity == units) && (point == true))
 			{
-				for (int subarrayX = i; subarrayX < i + r2; subarrayX++)
+				for (int subarrayX = 0; subarrayX < r2; subarrayX++)
 				{
-					for (int subarrayY = j; subarrayY < j + c2; subarrayY++)
+					for (int subarrayY = 0; subarrayY < c2; subarrayY++)
 					{
-						if (second_figure[subarrayX - i][subarrayY - j] == 1) fake_figure[subarrayX][subarrayY] = 1;
+						if (second_figure[subarrayX][subarrayY] == 1) fake_figure[subarrayX + i][subarrayY + j] = 1;
 					}
 				}
 				point = false;
 			}
 		}
 	}
-	colums = c1; rows = r1;
+
 	return fake_figure;
 	/*if (get_solve) return fake_figure;
 	else
@@ -236,34 +189,41 @@ void polymino::compare()
 {	    
 	for (int i = 0; i < n - 1; i++)
 	{
-		for (int j = 0; j < n - i - 1; j++) 
+		if (figures[i].get_c()*figures[i].get_r() < figures[i + 1].get_c()*figures[i + 1].get_r())
+		{ 
+			swap(figures[i], figures[i + 1]);
+		}
+			
+		if (figures[i].get_c()*figures[i].get_r() < figures[i + 1].get_c()*figures[i + 1].get_r())
 		{
-			if (figures[j].square() < figures[j + 1].square()) 
+			if (figures[i].square() < figures[i + 1].square())
 			{
-				swap(figures[j], figures[j + 1]);
+				swap(figures[i], figures[i + 1]);
 			}
 		}
 	}
+	cout << endl;
 
 	bool get_solve = false;
-
-	solvation = figures[0].array();
-	colums = figures[0].get_c(); rows = figures[0].get_r();
-
-	while (!get_solve)
-	{
-		for (int i = 0; i < (n - 1); i++)
-		{
-			solvation = mergeFigures(solvation, rows, colums, figures[i + 1].array(), figures[i + 1].get_r(), figures[i + 1].get_c());
-			f result(solvation, rows, colums);
-			if (result.plenum() == 0) get_solve = true;
-		}
-
-		
-
-	}
 	
-			
+	solvation = figures[0].array();
+	colums = figures[0].get_c(); 
+	rows = figures[0].get_r();
+
+	/*while (!get_solve)
+	{
+	}*/
+
+	for (int i = 0; i < (n - 1); i++)
+	{
+		//figures[i + 1].findAngle();
+		//figures[i + 1].print();
+		solvation = mergeFigures(solvation, rows, colums, figures[i + 1].array(), figures[i + 1].get_r(), figures[i + 1].get_c());
+		
+		//if (result.plenum() == 0) get_solve = true;
+	}
+	f result(solvation, rows, colums);
+	result.print();
 }
 
 void polymino::resultFile()
